1efa731290a4ffe8770bcd590ca0bde233d447a3
Gabriella Guerra <gabriellafguerra21@gmail.com>
2024-10-10 21:04:50 -0300

    database config

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
-from flask import Flask
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+db = SQLAlchemy(app)
+
+# Database Model
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.Text, nullable=True)
 
 @app.route('/')
 def index():
     return 'Welcome to my Index Page!'
+
+@app.route('/api/products/add', methods=['POST'])
+def add_product():
+    data = request.json()
+
+if __name__ == '__main__':
+    app.run(debug=True)
